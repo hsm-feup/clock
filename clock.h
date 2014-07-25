01a9b9d79a05d5412b07ef165a48e92e8a6c8be6
Helio Mendonca <hsm.feup@gmail.com>
2014-07-25 09:45:17 +0100

    Clock first commit

---
+++ clock.h
@@ -0,0 +1,15 @@
+/*
+ * clock.h
+ *
+ *  Created on: 25/07/2014
+ *      Author: Helio
+ */
+
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+void initClock();
+void cmdClock(char *msg);
+void procClock();
+
+#endif /* CLOCK_H_ */
